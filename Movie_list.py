82bec9bf061b1fe8d5befc835680a2b0135ac379
surgekalva7 <surgekalva7@gmail.com>
2022-05-23 21:22:41 -0400

    display selectedMovies

--- Movie_list.py
+++ Movie_list.py
@@ -1,20 +1,28 @@
+from bs4 import ResultSet
 import requests
 
-print("Please Enter the movie names: ")
+#print("Please Enter the movie names: ")
 
-title = input("Movie name: ")
+#title = input("Movie name: ")
 
-URL = "https://imdb-api.com/en/API/Search/k_z9p2w7dy/"+title
+#URL = "https://imdb-api.com/en/API/Search/k_z9p2w7dy/"+title
 
-PARAMS = {'title':title}
+#PARAMS = {'title':title}
 
-r = requests.get(url = URL, params = PARAMS)
+#r = requests.get(url = URL, params = PARAMS)
 
-data = r.json()
+#data = r.json()
 
-print(data)
+#print(data)
 
 
+def displaySelectedMovies(data_results):
+    for i in range(len(data_results)):
+        print('{}. {}'.format(i, data_results[i]['title']))
+    print
+
+def selectMovie(data_results):
+    print()
 
 
 
@@ -25,6 +33,14 @@ def main():
     error = False
     while(len(user_movie_list)==0 or error):
         user_movie_name = input("Enter the name of the movie: ")
+        URL = "https://imdb-api.com/en/API/Search/k_z9p2w7dy/"+user_movie_name
+        PARAMS = {'title':user_movie_name}
+        r = requests.get(url = URL, params = PARAMS)
+        data = r.json()
+        if(len(data['results'])>0):
+            selectMovie()
+
+
 
 
 if __name__ == "__main__":
