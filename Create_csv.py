4be53de67ee8b7dd3cd2fc046d0f671c0d7cb1df
surgekalva7 <surgekalva7@gmail.com>
2022-05-31 20:55:51 -0400

    Addition of the IMDb_webscraper.py

--- Create_csv.py
+++ Create_csv.py
@@ -1,7 +1,9 @@
+#@author: What is our team name?
+
 import requests
 
 import csv
-
+"""
 URL = "https://imdb-api.com/en/API/Top250Movies/k_z9p2w7dy"
 r = requests.get(url = URL)
 data = r.json()
@@ -21,3 +23,8 @@ with open('data.csv','w') as f:
     write = csv.writer(f)
     write.writerow(col)
     write.writerows(row)
+"""
+URL = "https://imdb-api.com/en/API/IMDbList/k_z9p2w7dy/ls004285275"
+r = requests.get(url = URL)
+data = r.json()
+print(data)
