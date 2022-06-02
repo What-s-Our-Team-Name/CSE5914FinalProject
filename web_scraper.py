e8ab40c0c262b313caeb111dae097466fc5e86e2
Andrew Coyle <coyleandrewi@gmail.com>
2022-06-02 15:29:52 -0400

    Updated web scraper

--- web_scraper.py
+++ web_scraper.py
@@ -3,26 +3,20 @@ import requests
 import csv
 import re
 
-root = 'https://www.imdb.com'
-search_url = '/search/title/?title_type=feature&sort=alpha,asc'
-response = requests.get(root+search_url)
-soup = BeautifulSoup(response.text, "html.parser")
-
-with open('test_data.csv', 'w') as f:
-    print(soup.find('div', class_='lister-item-content').prettify())
+def scrape_page():
     for movie in soup.findAll('div', class_='lister-item-content'):
         if movie.find(href=re.compile('title')):
             s = str(movie.find(href=re.compile('title')))
             ss1 = 'tt'
             ss2 = '/">'
-            print(s[s.find(ss1):s.find(ss2)])
-            print(movie.find(href=re.compile('title')).get_text())
+            title_id = s[s.find(ss1):s.find(ss2)]
+            title = movie.find(href=re.compile('title')).get_text()
         if movie.find('span', class_='lister-item-year text-muted unbold'):
-            print(movie.find('span', class_='lister-item-year text-muted unbold').get_text())
+            year = movie.find('span', class_='lister-item-year text-muted unbold').get_text()
         if movie.find('span', class_='runtime'):
-            print(movie.find('span', class_='runtime').get_text())
+            runtime = movie.find('span', class_='runtime').get_text()
         if movie.find('span', class_='genre'):
-            print(movie.find('span', class_='genre').get_text())
+            genres = movie.find('span', class_='genre').get_text()
         if movie.find('p', class_=''):
             s = movie.find('p', class_='').get_text()
             start = s.find('    Director:') + len('    Director:')
@@ -31,9 +25,18 @@ with open('test_data.csv', 'w') as f:
             start = s.find('    Stars:') + len('    Stars:')
             end = s.find('|')
             stars = s[start:].strip()
-            print(director)
-            print(stars)
-        print('--------------------------------------------------------------------------------')
+        
+
+with open('test_data.csv', 'w') as f:
+    root = 'https://www.imdb.com'
+    search_url = '/search/title/?title_type=feature&sort=alpha,asc'
+    response = requests.get(root+search_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    while soup.find('a', class_='lister-page-next next-page'):
+        scrape_page()
+        search_url = soup.find('a', class_='lister-page-next next-page').attrs['href']
+        response = requests.get(root+search_url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
     # search_url = soup.find('a', class_="lister-page-next next-page").get('href')
     # response = requests.get(root+search_url)
