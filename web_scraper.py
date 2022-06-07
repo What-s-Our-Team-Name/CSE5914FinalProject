b63e6fb5e6772838b8050f6be2586a3ee493e888
Andrew Coyle <coyleandrewi@gmail.com>
2022-06-07 13:38:39 -0400

    Started using split method

--- web_scraper.py
+++ web_scraper.py
@@ -5,7 +5,11 @@ import re
 from random import random
 import time
 
-def scrape_page():
+
+def randomSleep():
+    time.sleep(random())
+
+def scrape_page(writer):
     for movie in soup.findAll('div', class_='lister-item-content'):
         if movie.find(href=re.compile('title')):
             s = str(movie.find(href=re.compile('title')))
@@ -21,29 +25,26 @@ def scrape_page():
             genres = movie.find('span', class_='genre').get_text()
         if movie.find('p', class_=''):
             s = movie.find('p', class_='').get_text()
-            start = s.find('    Director:') + len('    Director:')
-            end = s.find('|')
-            director = s[start:end].strip()
-            start = s.find('    Stars:') + len('    Stars:')
-            end = s.find('|')
-            stars = s[start:].strip()
-        
+            print(s)
+            str_list = s.split()
+            print(str_list)
+            # start = s.find('Director:') + len('Director:')
+            # end = s.find('|')
+            # director = s[start:end].strip()
+            # start = s.find('Stars:') + len('Stars:')
+            # end = s.find('|')
+            # stars = s[start:].strip()
+        writer.writerow((title_id, title, year, runtime, genres, director, stars))       
 
-with open('test_data.csv', 'w') as f:
+with open('movie_data.csv', 'w') as f:
+    writer = csv.writer(f)
     root = 'https://www.imdb.com'
     search_url = '/search/title/?title_type=feature&sort=alpha,asc'
     response = requests.get(root+search_url)
     soup = BeautifulSoup(response.text, "html.parser")
-    while soup.find('a', class_='lister-page-next next-page'):
-        scrape_page()
-        search_url = soup.find('a', class_='lister-page-next next-page').attrs['href']
-        response = requests.get(root+search_url)
-        soup = BeautifulSoup(response.text, "html.parser")
-
-
-def randomSleep():
-    time.sleep(random())
-
-    # search_url = soup.find('a', class_="lister-page-next next-page").get('href')
+    #while soup.find('a', class_='lister-page-next next-page'):
+    scrape_page(writer)
+    randomSleep() 
+    # search_url = soup.find('a', class_='lister-page-next next-page').attrs['href']
     # response = requests.get(root+search_url)
     # soup = BeautifulSoup(response.text, "html.parser")
