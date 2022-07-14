67ce95feb3a6b223332edbf3fa5f997bac2f7568
surgekalva7 <surgekalva7@gmail.com>
2022-07-14 15:37:32 -0400

    database route backend

--- web_app/back_end/back_end.py
+++ web_app/back_end/back_end.py
@@ -1,19 +1,24 @@
 from flask import Flask
-from requests import request
+from flask import request
+import name_recommendations
+import requests
 
 app = Flask(__name__)
+
+user_movie_list = ["tt0317219"]
 
 @app.route("/imdb")
 def get_imdb_results():
     movie = request.args.get('user_movie')
     URL = "https://imdb-api.com/en/API/AdvancedSearch/k_z9p2w7dy"
     PARAMS = {'title':movie, 'title_type':'feature', 'release_date':',2021-01-01', 'languages':'en'} 
-    r = request.get(url = URL, params = PARAMS)
+    r = requests.get(url = URL, params = PARAMS)
     data = r.json()
     return data
 
 @app.route("/database")
 def get_database_results():
-    pass
+    rec_movies = name_recommendations.getRecommendations(user_movie_list)
+    return rec_movies
 
 app.run()
---
+++ web_app/back_end/name_recommendations.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+
+titles = pd.read_csv('recom_title.csv')
+titleids = pd.read_csv('recom_titleid.csv')
+sim_records = pd.read_csv('sim_record.csv')
+
+
+"""
+DESCRIPTION 
+    - Get the recommendations for the top 15 movies
+@parameters
+    user_movie_ids - the title_ids from the user movie list
+@return
+    - the top 15 movies that can be recommended given the user's choice
+"""
+def getRecommendations(user_movie_ids):
+    #user_movie_list,idss = ml.getMovieList()
+    result = {}
+    for user_movie_id in user_movie_ids:
+        recom_ids = titleids[titleids['title_id'] == user_movie_id].to_numpy().tolist()[0][1:]
+        #print(recom_ids)
+        sim_scores = sim_records[titleids['title_id'] == user_movie_id].to_numpy().tolist()[0][1:]
+        #print(recom_ids)
+        for i in range(15):
+            if recom_ids[i] in result:
+                result[recom_ids[i]] += sim_scores[i]
+            else:
+                result[recom_ids[i]] = sim_scores[i]
+    recom_list = sorted(list(result.keys()), key = lambda x: result[x], reverse = True)
+    recom_movies = [titles.loc[titleids['title_id'] == recom_id, 'title'].tolist()[0] for recom_id in recom_list]
+    movie_dict = {}
+    top15_movies = recom_movies[:15]
+    for i in range(len(top15_movies)):
+        movie_dict[i] = top15_movies[i]
+
+    return movie_dict
+
+
