f5bf3628344bda90f829cd9649ce2be06fce7aee
zhouyifan1997 <zyfsteven1@gmail.com>
2022-07-15 07:14:32 +0000

    [Deploy][Yifan] Add deploy script for backend server

--- web_app/server/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /root/CSE5914FinalProject/web_app/server/chatbot.service /etc/systemd/system/chatbot.service
-sudo systemctl daemon-reload
-pkill chatbot
-sudo systemctl start chatbot
